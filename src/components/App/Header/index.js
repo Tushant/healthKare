f4e070562869cf567afcb3d555a6ce12f908fd40
Tushant <programmertushant@gmail.com>
2018-12-26 19:26:13 +0545

    build file updated

--- src/components/App/Header/index.js
+++ src/components/App/Header/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import styled from 'styled-components';
 
+import LogoIcon from 'assets/icons/Logo.svg';
 import Menu from './Menu';
 import Search from './Search';
-
-import styled from 'styled-components';
-import LogoIcon from 'assets/icons/Logo.svg';
 
 const
   Wrapper = styled.div`
